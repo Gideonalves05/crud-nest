2846fd71e97202f4e1fa06e2ed83dc72bf676253
Gideon Alves <gideon.fame.94@gmail.com>
2024-04-20 16:28:26 -0300

    primeiro crud em nest

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+
+  
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
+
---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+
+constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = new this.UserModel(CreateUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.UserModel.find();
+  }
+
+findOne(id: string) {
+    return this.UserModel.findById(id);
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.UserModel.findByIdAndUpdate({
+        _id: id,
+    },{
+        updateUserDto,
+        
+    },{
+        new: true,
+    },
+  );
+  }
+
+  remove(id: string) {
+    return this.UserModel.deleteOne({
+      _id: id,
+    })
+    .exec();
+  }
+}
