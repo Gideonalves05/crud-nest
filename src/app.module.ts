7614fa745635f62292b76df98a9f500d4b6f3d74
Gideon Ipiranga Alves <141575454+Gideonalves05@users.noreply.github.com>
2024-05-22 14:58:32 -0300

    Update app.module.ts/moongose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://gideonfame94:GidsDev%4003122004@cluster0.jywljhv.mongodb.net/'), UsersModule],
 
   
   controllers: [],
